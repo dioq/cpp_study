c1bd1e2cbf8ae47f39476b3943f4b64a7b7338c6
Dio <zhendong2011@live.cn>
2019-11-22 10:45:14 +0800

    CPP

---
+++ CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP
+//
+//  Created by hello on 2019/11/22.
+//  Copyright © 2019 Dio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
